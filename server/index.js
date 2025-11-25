46962f6945e125431194acf978b99060a6f15a31
bandamupendar <bandamupendar4868@gmail.com>
2025-11-26 00:04:26 +0530

    frontend push

--- server/index.js
+++ server/index.js
@@ -4,13 +4,13 @@ import path from "path"; // Ensure the path module is imported
 const app = express();
 import bodyParser from "body-parser";
 import cors from "cors";
-import axios from 'axios'; 
+import axios from 'axios';
 
 // Enable CORS
 app.use(cors({
-    origin: "*", // Allow requests from this origin
-    methods: ["GET", "POST"], // Specify allowed HTTP methods
-    allowedHeaders: ["Content-Type"] // Specify allowed headers
+  origin: "*", // Allow requests from this origin
+  methods: ["GET", "POST"], // Specify allowed HTTP methods
+  allowedHeaders: ["Content-Type"] // Specify allowed headers
 }));
 
 // Serve static files from the React app's build directory
@@ -23,19 +23,19 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 // Proxy route to the backend
 app.post('/customer/customerDetails', async (req, res) => {
-    try {
-      const data = req.body;
-      // const response = await axios.post('https://backend.hyderabad-packers-movers.in/customer/customerDetails', data);
-      const response = await axios.post('http://65.2.190.41:8080/customer/customerDetails', data);
-      // Send the JSON response from Spring Boot to the frontend
-      res.status(200).json(response.data);
-    } catch (error) {
-      console.error("Error communicating with Spring Boot:", error.message);
-      res.status(500).json({ error: 'Internal Server Error' });
-    }
-  });
+  try {
+    const data = req.body;
+    // const response = await axios.post('https://backend.hyderabad-packers-movers.in/customer/customerDetails', data);
+    const response = await axios.post('http://localhost:8080/customer/customerDetails', data);
+    // Send the JSON response from Spring Boot to the frontend
+    res.status(200).json(response.data);
+  } catch (error) {
+    console.error("Error communicating with Spring Boot:", error.message);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
 
-  // Catch-all route to serve the React app for non-API requests
+// Catch-all route to serve the React app for non-API requests
 app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "build", "index.html"));
 });
@@ -44,5 +44,5 @@ app.get("*", (req, res) => {
 // Start the BFF server
 const PORT = 5000;
 app.listen(PORT, () => {
-    console.log(`BFF server running on http://localhost:${PORT}`);
+  console.log(`BFF server running on http://localhost:${PORT}`);
 });
